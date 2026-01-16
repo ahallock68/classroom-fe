0285d1b91a9c1a4d461a170b6b19906665196f18
Andrew Hallock <ahallock68@yahoo.com>
2026-01-16 12:47:31 -0800

    implement data-provider

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+export const DEPARTMENTS = [
+  "Mathematics",
+  "Science",
+  "History",
+  "Literature",
+  "Art",
+  "Physical Education",
+  "Computer Science",
+  "Biology",
+  "Chemistry",
+  "Physics",
+];
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
+  label: dept,
+  value: dept,
+}));
